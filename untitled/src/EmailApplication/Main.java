88efcfeaa57cdff9be05d45451d3decaa6a22cfa
MOhamed Abdallah <119700010+MO-87@users.noreply.github.com>
2023-12-04 16:12:34 +0200

    Add files via upload

---
+++ untitled/src/EmailApplication/Main.java
@@ -0,0 +1,19 @@
+package EmailApplication;
+
+import javax.swing.text.Style;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
+
+
+@SuppressWarnings("ALL")
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+
+    }
+}
