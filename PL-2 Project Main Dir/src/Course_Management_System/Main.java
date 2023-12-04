e7a066a3effd1092a2d759d0973f6273010c65e5
MOhamed Abdallah <119700010+MO-87@users.noreply.github.com>
2023-12-04 16:24:27 +0200

    First commit to add the main project files.

---
+++ PL-2 Project Main Dir/src/Course_Management_System/Main.java
@@ -0,0 +1,10 @@
+package Course_Management_System;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("This is the start for our project :)");
+    }
+}
