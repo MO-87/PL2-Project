ef0c152d15e9d792d20623133694247983a8fb66
MOhamed Abdallah <119700010+MO-87@users.noreply.github.com>
2023-12-17 02:30:18 +0200

    Update Main.java

--- PL-2 Project Main Dir/src/Course_Management_System/Main.java
+++ PL-2 Project Main Dir/src/Course_Management_System/Main.java
@@ -1,11 +1,12 @@
 package Course_Management_System;
 
 import java.io.*;
-import java.util.Scanner;
+import java.text.*;
+import java.util.*;
 
 public class Main
 {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args) throws IOException, ParseException
     {
         Menu.Main_Menu();
     }
